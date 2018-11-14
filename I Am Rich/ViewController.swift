6fc547e5e7c1f15e9c6e926b7825398029acb505
Trieu Quy Tam <TamTrieu@Trieus-Air.lan>
2018-11-14 21:19:48 +0700

    Initial commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Trieu Quy Tam on 11/5/18.
+//  Copyright © 2018 Trieu Quy Tam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
